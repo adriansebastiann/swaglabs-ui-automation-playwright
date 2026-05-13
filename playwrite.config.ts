9f52b8fcc0c9e30d033189b0ea4bb3212576c8d6
Adrian-Sebastian Alexoaie <adrianalexoaie@yahoo.com>
2026-05-13 21:11:41 +0300

    try to fix config for CI

--- playwrite.config.ts
+++ playwrite.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from '@playwright/test';
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests',

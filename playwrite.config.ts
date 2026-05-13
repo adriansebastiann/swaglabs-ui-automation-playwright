ef121485629b12d6b538a2e0a063e5e3b8ef4e9e
Adrian-Sebastian Alexoaie <adrianalexoaie@yahoo.com>
2026-05-13 21:17:11 +0300

    attempt 2 to fix CI

--- playwrite.config.ts
+++ playwrite.config.ts
@@ -4,19 +4,37 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   retries: 0,
+
   use: {
     baseURL: process.env.BASE_URL || 'https://www.saucedemo.com',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry'
   },
+
   reporter: [
     ['html'],
     ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
+
   projects: [
-    { name: 'chromium', use: { browserName: 'chromium' } },
-    { name: 'firefox', use: { browserName: 'firefox' } },
-    { name: 'webkit', use: { browserName: 'webkit' } }
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium'
+      }
+    },
+    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox'
+      }
+    },
+    {
+      name: 'webkit',
+      use: {
+        browserName: 'webkit'
+      }
+    }
   ]
 });
